cba0d925ec982b6894bcaa191a7a8f3bbcc0c20f
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-09-08 16:17:29 +0300

    Applied font and added some styles and colors to text and button

--- test_flutter/lib/main.dart
+++ test_flutter/lib/main.dart
@@ -6,13 +6,24 @@ void main() {
         appBar: AppBar(
           title: Text("Hello World"),
           centerTitle: true,
+          backgroundColor: Colors.lightGreen[600],
         ),
         body: Center(
-          child:  Text("This is my first flutter app")
+          child:  Text(
+              "This is my first flutter app",
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.blueGrey,
+              fontFamily: 'IndieFlower',
+            ),
+          )
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {  },
           child: Text("Click"),
+          backgroundColor: Colors.lightGreen[600],
         ),
       ),
   ));
