7ee529f76962b7ca875875247acce4fb3af9206b
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-09-08 19:58:36 +0300

    Added the stateless widget

--- test_flutter/lib/main.dart
+++ test_flutter/lib/main.dart
@@ -2,30 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello World"),
-          centerTitle: true,
-          backgroundColor: Colors.lightGreen[600],
-        ),
-        body: Center(
-          child:  Text(
-              "This is my first flutter app",
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.blueGrey,
-              fontFamily: 'IndieFlower',
-            ),
-          )
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {  },
-          child: Text("Click"),
-          backgroundColor: Colors.lightGreen[600],
-        ),
-      ),
+      home: Home(),
   ));
 }
-
+ class Home extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       appBar: AppBar(
+         title: Text("Hello World"),
+         centerTitle: true,
+         backgroundColor: Colors.lightGreen[600],
+       ),
+       body: Center(
+           child:  Text(
+             "This is my first flutter app",
+             style: TextStyle(
+               fontSize: 20.0,
+               fontWeight: FontWeight.bold,
+               letterSpacing: 2.0,
+               color: Colors.blueGrey,
+               fontFamily: 'IndieFlower',
+             ),
+           )
+       ),
+       floatingActionButton: FloatingActionButton(
+         onPressed: () {  },
+         child: Text("Click Me"),
+         backgroundColor: Colors.lightGreen[600],
+       ),
+     );
+   }
+ }
+ 
