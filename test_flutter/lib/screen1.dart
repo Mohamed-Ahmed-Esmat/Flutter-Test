b7a19f265c2d2fc958cb53e87857a35577466af0
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-09-10 16:24:10 +0300

    Added new files for testing navigation between screens

--- test_flutter/lib/screen1.dart
+++ test_flutter/lib/screen1.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:test_flutter/full_screen_image.dart';
+import 'package:test_flutter/screen2.dart';
 
 class Screen1 extends StatelessWidget {
   const Screen1({super.key});
@@ -11,15 +14,32 @@ class Screen1 extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.lightGreen[600],
       ),
-      body: Align(
-          alignment: Alignment.topCenter,
-          child: Text(
-            "Hey, Just Testing",
-            style: ,
-          )),
+      body: Column(
+        children: [
+          InkWell(
+            onTap: () {
+              Get.to(FullScreenImage());
+            },
+            child: Image.asset(
+              'assets/Screenshot (1).png',
+              width: double.infinity,
+              height: 500,
+              // fit: BoxFit.fill,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Hello World",
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text("Click"),
+        onPressed: () {
+          Get.to(Screen2());
+        },
+        child: Text("Next"),
         backgroundColor: Colors.lightGreen[600],
       ),
     );
